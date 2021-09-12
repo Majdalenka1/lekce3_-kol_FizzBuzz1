acc7b0b0563cbb29ce0d0835ff8b73741c1980d5
Mia Marie Pelzlova <87334389+Majdalenka1@users.noreply.github.com>
2021-09-12 07:47:32 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.engeto;
+
+public class Main {
+
+    final static String FIZZ = "fizz";
+    final static String BUZZ = "buzz";
+    final static String FIZZBUZZ = "fizzBuzz";
+
+    public static void main (String [] agrs) {
+        fizzBuzzJava();
+    }
+
+    public  static void fizzBuzzJava()
+    {
+        for (int i = 1; i <= 100; i++){
+            if ((i % 3 == 0) && (i % 5 == 0))
+            System.out.println(FIZZBUZZ);
+            else if (i % 3 == 0) System.out.println(FIZZBUZZ);
+            else if (i % 5 == 0) System.out.println(FIZZ);
+            else System.out.println(i);
+
+        }
+    }
+}
